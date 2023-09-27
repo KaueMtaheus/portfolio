6f05086175efb07d65475b7b26940d9a26b63239
Kauê Matheus <kauematheus7@hotmail.com>
2023-09-26 21:02:37 -0300

    novo

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//funcao para alternar meu menu 
 function toggleMenu() {
     const navbar = document.querySelector('.navbar');
     const navigation = document.querySelector('.navegacao-primaria');
@@ -5,3 +7,32 @@ function toggleMenu() {
     navbar.classList.toggle('active');
     navigation.classList.toggle('active');
 }
+
+
+//funcao para mostrar/ ocultar projetos 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const showMoreButton = document.getElementById("show-more");
+    const hiddenProjects = document.querySelectorAll(".hidden");
+
+    let isShowingMore = false;
+
+    showMoreButton.addEventListener("click", function () {
+        if (isShowingMore) {
+            // Esconde projetos adicionais
+            hiddenProjects.forEach(project => {
+                project.style.display = "none";
+            });
+            showMoreButton.textContent = "Ver mais projetos";
+        } else {
+            // Mostra projetos adicionais
+            hiddenProjects.forEach(project => {
+                project.style.display = "inline-block"; // Altere para "inline-block" para exibir lado a lado
+            });
+            showMoreButton.textContent = "Ver menos";
+        }
+
+        isShowingMore = !isShowingMore;
+    });
+});
+
